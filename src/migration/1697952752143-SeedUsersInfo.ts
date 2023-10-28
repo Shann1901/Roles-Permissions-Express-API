8c461ed93dc54f1415d267bf28acbfc0212cbfe5
Shantanu Nigam <shantanunigam1901@gmail.com>
2023-10-28 14:55:26 +0530

    new migration updates

--- src/migration/1697952752143-SeedUsersInfo.ts
+++ src/migration/1697952752143-SeedUsersInfo.ts
@@ -11,22 +11,44 @@ export class SeedUsersInfo1697952752143 implements MigrationInterface {
     readonly dataSource: DataSource = dataSourceInstance
     public async up(queryRunner: QueryRunner): Promise<void> {
         try {
+            const permissionRepository = this.dataSource.getRepository(Permission)
+            const roleRepository = this.dataSource.getRepository(Role)
+            const userRepository = this.dataSource.getRepository(User)
 
             for (const userData of usersData) {
                 if (userData.permissions) {
                     const permissonsToSave = []
                     for (const permission of userData.permissions) {
-                        const permissionInstance = new Permission()
-                        permissionInstance.permission = permission
-                        permissionInstance.description = RolePermissions.getPermissionDescription(permission)
-                        permissonsToSave.push(permissionInstance)
+                        let permissionInstance
+                        const permissionRecord = await permissionRepository.find({
+                            where: {
+                                permission: permission
+                            }
+                        }) 
+                        if (!permissionRecord[0]) {
+                            permissionInstance = new Permission()
+                            permissionInstance.permission = permission
+                            permissionInstance.description = RolePermissions.getPermissionDescription(permission)
+                        }
+                        permissonsToSave.push(permissionRecord[0] ?? permissionInstance)
+                       
                     }
 
-                    const roleInstance = new Role()
-                    roleInstance.roleName = userData.role
-                    roleInstance.roleDescription = RolePermissions.getRoleDescription(userData.role)
 
-                    const userRepository = this.dataSource.getRepository(User)
+                    let roleInstance = null
+                    const roleRecord = await roleRepository.find({
+                        where: {
+                            roleName: userData.role
+                        }
+                    })
+
+                    if (!roleRecord[0]) {
+                        roleInstance = new Role()
+                        roleInstance.roleName = userData.role
+                        roleInstance.roleDescription = RolePermissions.getRoleDescription(userData.role)
+                    }
+
+                    
                     const userInstance = new User()
                     userInstance.firstname = userData.firstName
                     userInstance.lastName = userData.lastName
@@ -35,11 +57,17 @@ export class SeedUsersInfo1697952752143 implements MigrationInterface {
                     userInstance.age = userData.age
 
                     // Relations connections (one-to-many, many-to-many)
-                    userInstance.role = roleInstance
-                    roleInstance.permissions = [...permissonsToSave]
+                    userInstance.role = roleRecord[0] ?? roleInstance
+
+                    if (roleInstance) {
+                        roleInstance.permissions = [...permissonsToSave]
+                    } else {
+                        roleRecord[0].permissions = [...permissonsToSave]
+                    }    
+                    
     
                     // With cascade: true on the entities, ater saving user, roles and permissions will be auto-saved by TypeORM
-                    userRepository.save(userInstance)
+                    await userRepository.save(userInstance)
                 }
 
             }
@@ -66,22 +94,21 @@ export class SeedUsersInfo1697952752143 implements MigrationInterface {
             }
         })
 
+
         for (const user of allUsers) {
             const role = user.role
             const permissions = role.permissions
            
             for (const permission of permissions) {
-                userPermissionRepository.delete({
+               await userPermissionRepository.delete({
                     roleId: role.id,
                     permissionId: permission.id
                 })
-                permissionRepository.delete({
-                    id: permission.id
-                })
             }
-            userRepository.delete({ id: user.id}) 
-            roleRepository.delete({ id: role.id }) 
+            await userRepository.delete({ id: user.id}) 
         }
+        await roleRepository.delete({})
+        await permissionRepository.delete({})
     }
 
 }
